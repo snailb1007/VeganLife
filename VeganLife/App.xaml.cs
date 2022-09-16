f0d5ee330887419da8228da55dde68b3c2b48190
DC-VIETIS\thiennv <thiennv@vietis.com.vn>
2022-09-16 17:07:31 +0700

    Add project files.

---
+++ VeganLife/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VeganLife;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ VeganLife/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VeganLife;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
