6be99674e813bd6aa0cedd77621a46f7036a704a
DC-VIETIS\thiennv <thiennv@vietis.com.vn>
2022-09-26 17:25:03 +0700

    logic: add value

--- VeganLife/ViewModels/BMICalculatorViewModel.cs
+++ VeganLife/ViewModels/BMICalculatorViewModel.cs
@@ -6,12 +6,18 @@ namespace VeganLife.ViewModels
     public partial class BMICalculatorViewModel : ObservableObject
     {
         [ObservableProperty]
-        bool _isDisplayedSexDetail;
+        bool _isDisplayedSexDetail = true;
+
+        [ObservableProperty]
+        string _weightValue;
+
+        [ObservableProperty]
+        string _ageValue;
 
         [RelayCommand]
         void HelpSexDetail()
         {
-            _isDisplayedSexDetail = !_isDisplayedSexDetail;
+            IsDisplayedSexDetail = !IsDisplayedSexDetail;
         }
 
         public BMICalculatorViewModel()
@@ -20,5 +26,22 @@ namespace VeganLife.ViewModels
 
         private void Init()
         { }
+
+        partial void OnWeightValueChanged(string value)
+        {
+            if (value.Length < 6)
+                return;
+            var abc = string.Format("0:0.00", value);
+            bool isSuccess = float.TryParse(value, out float x);
+            string result = x.ToString();
+            if (!isSuccess || !result.Equals(value))
+                return;
+            WeightValue = result;
+        }
+
+        partial void OnAgeValueChanged(string value)
+        {
+            Console.WriteLine($"thien==>{_ageValue}");
+        }
     }
 }
