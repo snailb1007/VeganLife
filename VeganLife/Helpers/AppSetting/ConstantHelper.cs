accc242fc36e6ac82a8d38e4fed3d00710480a30
DESKTOP-8PT3G6R\Admin <thiennv@vietis.com.vn>
2022-09-28 23:24:50 +0700

    add helper

---
+++ VeganLife/Helpers/AppSetting/ConstantHelper.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VeganLife.Helpers.AppSetting
+{
+    public static class ConstantHelper
+    {
+        public static class ThemeInfo
+        {
+            public const string UniverseDarkLink = "https://i.imgur.com/IHnQcdF.jpg";
+            public static bool IsDarkMode { get; set; }
+        }
+    }
+}
