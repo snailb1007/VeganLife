8ceac0a65a458677600f337a46d1e0f5e82cc67b
thien <thiennguyen1007@gmail.com>
2023-01-13 00:19:00 +0700

    update ui chart

--- VeganLife/ViewModels/ContentViewModels/RationPlanViewModel.cs
+++ VeganLife/ViewModels/ContentViewModels/RationPlanViewModel.cs
@@ -1,13 +1,30 @@
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.VisualElements;
 
 namespace VeganLife.ViewModels.ContentViewModels;
 
 public partial class RationPlanViewModel : ObservableObject
 {
-    [ObservableProperty]
-    public ISeries[] _series;
+    public ISeries[] Series { get; private set; } =
+        {
+            new PieSeries<float> { Values = new List<float> { 3 }, InnerRadius = 50, MaxOuterRadius = 0.9, Name = "Bữa sáng" },
+            new PieSeries<float> { Values = new List<float> { 4 }, InnerRadius = 50, MaxOuterRadius = 1.0, Name = "Bữa trưa" },
+            new PieSeries<float> { Values = new List<float> { 2.5f }, InnerRadius = 50, MaxOuterRadius = 0.8, Name = "Bữa tối" },
+            new PieSeries<float> { Values = new List<float> { 0.5f }, InnerRadius = 50, MaxOuterRadius = 0.7, Name = "Bữa phụ" }
+        };
+
+    public LabelVisual Title { get; private set; } =
+        new LabelVisual
+        {
+            Text = "Phân bổ năng lượng các bữa ăn trong ngày",
+            TextSize = 50,
+            Padding = new LiveChartsCore.Drawing.Padding(1),
+            Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+        };
 
     public RationPlanViewModel()
     {
@@ -16,13 +33,10 @@ public partial class RationPlanViewModel : ObservableObject
 
     void Init()
     {
-        Series = new ISeries[]
-            {
-                new PieSeries<double> { Values = new double[] { 2 } },
-                new PieSeries<double> { Values = new double[] { 4 } },
-                new PieSeries<double> { Values = new double[] { 1 } },
-                new PieSeries<double> { Values = new double[] { 4 } },
-                new PieSeries<double> { Values = new double[] { 3 } }
-            };
+        foreach (var item in Series)
+        {
+            var x = item as PieSeries<float>;
+            x.DataLabelsPosition = LiveChartsCore.Measure.PolarLabelsPosition.Outer;
+        }
     }
 }
