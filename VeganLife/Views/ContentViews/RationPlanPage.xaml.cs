a4e8e179bf126236f7b5cf799dd840aadcc5cff0
thien <thiennguyen1007@gmail.com>
2023-01-30 21:45:21 +0700

    update

--- VeganLife/Views/ContentViews/RationPlanPage.xaml.cs
+++ VeganLife/Views/ContentViews/RationPlanPage.xaml.cs
@@ -9,4 +9,10 @@ public partial class RationPlanPage : ContentPage
 		InitializeComponent();
         BindingContext = vm;
     }
+
+    private void PieChart_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        var chart = sender as LiveChartsCore.SkiaSharpView.Maui.PieChart;
+        Console.WriteLine($"thien==>{chart?.Width} {chart?.Height}");
+    }
 }
