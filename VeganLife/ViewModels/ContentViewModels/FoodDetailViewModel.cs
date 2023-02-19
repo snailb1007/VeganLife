b64f7e707dac8edd61953c10713a52c562c6259f
thien <thiennguyen1007@gmail.com>
2023-02-19 23:27:14 +0700

    update for food & detail page

---
+++ VeganLife/ViewModels/ContentViewModels/FoodDetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using VeganLife.Data.FireBaseData;
+using VeganLife.Models.FoodModel;
+
+namespace VeganLife.ViewModels.ContentViewModels
+{
+    public partial class FoodDetailViewModel : BaseViewModel, IQueryAttributable
+    {
+        [ObservableProperty]
+        FoodPreviewModel _foodPreview;
+
+        [ObservableProperty]
+        FoodDetailModel _foodDetail;
+        public FoodDetailViewModel() { }
+
+        public async void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query == null) { return; }
+            FoodPreview = query["SelectedFood"] as FoodPreviewModel;
+            FoodDetail = await FirebaseRealtimeData.GetFoodDetail(FoodPreview.Id ?? string.Empty);
+        }
+    }
+}
