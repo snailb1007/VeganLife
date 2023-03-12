f26d86674c1921ec93240284e221a9b203c4d46e
thien <thiennguyen1007@gmail.com>
2023-03-12 23:33:48 +0700

    update for search handler

--- VeganLife/Views/Controls/FuntionAppSearchHandler.cs
+++ VeganLife/Views/Controls/FuntionAppSearchHandler.cs
@@ -1,9 +1,30 @@
-﻿namespace VeganLife.Views.Controls
+﻿using System.Collections.Generic;
+using VeganLife.Helpers;
+using VeganLife.Models.FoodModel;
+
+namespace VeganLife.Views.Controls
 {
     public class FuntionAppSearchHandler : SearchHandler
     {
+        public bool IsSettingPage { private get; set; }
+
+        public static BindableProperty FoodPreviewsProperty = BindableProperty.Create(
+            propertyName: "FoodPreviews",
+            declaringType: typeof(MainPage),
+            returnType: typeof(IEnumerable<FoodPreviewModel>),
+            defaultValue: default(IEnumerable<FoodPreviewModel>));
+        public IEnumerable<FoodPreviewModel> FoodPreviews
+        {
+            get => (IEnumerable<FoodPreviewModel>)GetValue(FoodPreviewsProperty);
+            set => SetValue(FoodPreviewsProperty, value);
+        }
 
         public Type SelectedItemNavigationTarget { get; set; }
+
+        public FuntionAppSearchHandler()
+        {
+            ClearPlaceholderCommand = new Command(() => Query = string.Empty);
+        }
 
         protected override void OnQueryChanged(string oldValue, string newValue)
         {
@@ -15,19 +36,40 @@
             }
             else
             {
-                ItemsSource = (Shell.Current as AppShell).Routes
-                    .Where(item => item.Key.ToLower().Contains(newValue.ToLower()));
+                if (IsSettingPage)
+                {
+                    ItemsSource = (Shell.Current as AppShell).Routes
+                        .Where(item => item.Key.ToLower().Contains(newValue.ToLower()));
+                }
+                else
+                {
+                    if (FoodPreviews?.Any() ?? false)
+                        ItemsSource = FoodPreviews
+                            .Where(f => f.Name.ToLower().Contains(newValue.ToLower()))
+                            .ToList<FoodPreviewModel>();
+                }
+
             }
+
+            TextColor = ((IEnumerable<FoodPreviewModel>)ItemsSource)?.Any() ?? false ?
+                Colors.SkyBlue : Colors.Red;
         }
 
         protected override async void OnItemSelected(object item)
         {
             base.OnItemSelected(item);
-
+            if (this.IsFocused)
+                this.Unfocus();
             // Let the animation complete
-            await Task.Delay(1000);
-
-            ShellNavigationState state = (App.Current.MainPage as Shell).CurrentState;
+            await Task.Delay(500);
+            if (IsSettingPage)
+            {
+                ShellNavigationState state = (App.Current.MainPage as Shell).CurrentState;
+            }
+            else
+            {
+                await ServicesHelper.GetService<INavigationService>().NavigateToFoodDetail(item);
+            }
             // The following route works because route names are unique in this application.
             //await Shell.Current.GoToAsync($"{GetNavigationTarget()}?name={((Animal)item).Name}");
         }
