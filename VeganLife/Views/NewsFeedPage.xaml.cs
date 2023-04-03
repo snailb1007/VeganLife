1527c85e773ae1a7d6d83c9601dad382750077aa
thien <thiennguyen1007@gmail.com>
2023-04-03 23:07:39 +0700

    newsFeed: new ui

--- VeganLife/Views/NewsFeedPage.xaml.cs
+++ VeganLife/Views/NewsFeedPage.xaml.cs
@@ -2,11 +2,9 @@ namespace VeganLife.Views;
 
 public partial class NewsFeedPage : ContentPage
 {
-	public NewsFeedPage()
-	{ }
 	public NewsFeedPage(NewsFeedViewModel vm)
 	{
-		InitializeComponent();
-		BindingContext = vm;
+        BindingContext = vm;
+        InitializeComponent();
 	}
 }
