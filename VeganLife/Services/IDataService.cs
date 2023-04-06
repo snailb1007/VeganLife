faafde779ce9514743fe2aff93d17445b8172721
thien <thiennguyen1007@gmail.com>
2023-04-06 23:49:19 +0700

    new feed: new logic for load & improve performace

--- VeganLife/Services/IDataService.cs
+++ VeganLife/Services/IDataService.cs
@@ -8,5 +8,6 @@ namespace VeganLife.Services
         Task<FoodDetailModel> GetFoodDetail(string id);
         Task<IEnumerable<FoodMenuCategoryModel>> GetFoodMenu();
         Task<IEnumerable<VitaminModel>> GetVitamins();
+        Task<List<Item>> LoadGoogleNews(string uri);
     }
 }
---
+++ VeganLife/messages/NewFeedsLoadedMess.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace VeganLife.messages
+{
+    public class NewFeedsLoadedMess
+    {
+    }
+}
