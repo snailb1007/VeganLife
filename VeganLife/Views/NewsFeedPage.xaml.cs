38fe35f023214605dbae8ef7f48c3f71f4fe92dc
thien <thiennguyen1007@gmail.com>
2023-04-23 22:56:48 +0700

    update fix bug load newfeed

--- VeganLife/Views/NewsFeedPage.xaml.cs
+++ VeganLife/Views/NewsFeedPage.xaml.cs
@@ -16,13 +16,4 @@ public partial class NewsFeedPage : ContentPage
         _viewModel.SelectDiscoveryMenuCommand.Execute(e.CurrentSelection);
         collectionFeeds.ScrollTo(0, 0);
     }
-
-    private void collectionFeeds_PropertyChanged(object sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName.Equals("ItemSource"))
-        {
-            collectionFeeds.ItemsSource = null;
-            collectionFeeds.ItemsSource = _viewModel.Feeds;
-        }
-    }
 }
