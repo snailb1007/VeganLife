6e3f0c1cd7c6fb1e167ea23467f0ebedbc216192
thien <thiennguyen1007@gmail.com>
2023-05-14 23:03:23 +0700

    update font & loading ui

--- VeganLife/MauiProgram.cs
+++ VeganLife/MauiProgram.cs
@@ -26,6 +26,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("PlayfairDisplay-SemiBold.ttf", "PlayfairDisplaySemiBold");
                 fonts.AddFont("FontAwesome6FreeBrands.otf", "FontAwesomeBrands");
                 fonts.AddFont("FontAwesome6FreeRegular.otf", "FontAwesomeRegular");
                 fonts.AddFont("FontAwesome6FreeSolid.otf", "FontAwesomeSolid");
--- VeganLife/Views/FoodTab/FoodDetailPage.xaml.cs
+++ VeganLife/Views/FoodTab/FoodDetailPage.xaml.cs
@@ -2,11 +2,60 @@ using VeganLife.ViewModels.ContentViewModels;
 
 namespace VeganLife.Views.FoodTab;
 
-public partial class FoodDetailPage : ContentPage
+public partial class FoodDetailPage : ContentPage, INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+    Thickness _marginTopContent;
+    public Thickness MarginTopContent
+    {
+        get => _marginTopContent;
+        set
+        {
+            _marginTopContent = value;
+            OnPropertyChanged(nameof(MarginTopContent));
+        }
+    }
+
 	public FoodDetailPage(FoodDetailViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 	}
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    double _imgHeight;
+    private void Image_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+		if (e.PropertyName.Equals("Height"))
+		{
+			var img = sender as Image;
+			if (img?.Height > 0)
+			{
+				_imgHeight = img.Height;
+            }
+		}
+    }
+
+    double _frameTitleHeight;
+    private void Frame_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+
+        if (e.PropertyName.Equals("Height"))
+        {
+            var frame = sender as Frame;
+            if (frame?.Height > 0)
+            {
+                _frameTitleHeight = frame.Height;
+                if (_imgHeight > 0)
+                {
+                    //var centerFrame = _frameTitleHeight / 2;
+                    MarginTopContent = new Thickness(0, (int)(_imgHeight - _frameTitleHeight), 0, 0);
+                }
+            }
+        }
+    }
 }
