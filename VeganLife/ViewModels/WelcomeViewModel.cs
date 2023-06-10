6a033939c86f790c918597260b6009ce979c5309
Trinh Bac <trinhgiachang@gmail.com>
2023-06-10 18:07:32 +0700

    create ui WelcomePage

---
+++ VeganLife/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeganLife.ViewModels
+{
+    public class WelcomeImage
+    {
+        public WelcomeImage(string image)
+        {
+            this.Image = image;
+        }
+        public string Image { get; set; }
+    }
+
+    public partial class WelcomeViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        IList<WelcomeImage> _listImage;
+        
+        public WelcomeViewModel(INavigationService navigationService, IDataService dataService) : base(navigationService, dataService)
+        {
+            Init();
+        }
+      
+        void Init()
+        {
+            ListImage = new List<WelcomeImage>
+            {
+                new WelcomeImage("welcome1.jpg"),
+                new WelcomeImage("welcome2.jpg"),
+            };
+        }
+    }
+}
