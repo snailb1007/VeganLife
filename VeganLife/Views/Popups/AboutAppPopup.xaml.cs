b1c47b2949623cdc2ec76c7a439982fa4d92064c
thien <thiennguyen1007@gmail.com>
2023-07-23 22:30:32 +0700

    update: logic for about app popup

--- VeganLife/Views/Popups/AboutAppPopup.xaml.cs
+++ VeganLife/Views/Popups/AboutAppPopup.xaml.cs
@@ -10,4 +10,19 @@ public partial class AboutAppPopup : Popup
 		rootGrid.WidthRequest = App.MainSize * 0.8;
         lbVersion.Text = AppInfo.Current.VersionString;
 	}
+
+    private void TermsAndConditions_Tapped(object sender, TappedEventArgs e)
+    {
+        Browser.Default.OpenAsync("https://veganhealthies.wordpress.com/2023/07/23/dieu-khoan-dieu-kien/");
+    }
+
+    private void PrivacyPolicy_Tapped(object sender, TappedEventArgs e)
+    {
+        Browser.Default.OpenAsync("https://veganhealthies.wordpress.com/2023/07/23/chinh-sach-quyen-rieng-tu/");
+    }
+
+    private void Close_Clicked(object sender, EventArgs e)
+    {
+        this.Close();
+    }
 }
