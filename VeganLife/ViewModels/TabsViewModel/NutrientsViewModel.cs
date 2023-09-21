47155f60a053360659a91a70c65a6a9bd7b9fa18
TUNB1 <tunb1@vietis.com.vn>
2023-09-21 23:11:41 +0700

    add Tabs Report

---
+++ VeganLife/ViewModels/TabsViewModel/NutrientsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeganLife.ViewModels.TabsViewModel
+{
+    public partial class NutrientsViewModel : BaseViewModel
+    {
+        public NutrientsViewModel() 
+            : base()
+        {
+        }
+    }
+}
