d264c1a0f95bd6f276c7e46ae7c6c00dabef7289
thien <thiennguyen1007@gmail.com>
2023-09-25 00:08:00 +0700

    update

---
+++ VeganLife/Helpers/Converter/TitleForHyperlinkConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace VeganLife.Helpers.Converter
+{
+    internal class TitleForHyperlinkConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var rootString = value as string;
+                value = rootString.Substring(rootString.IndexOf("["), rootString.IndexOf("]"));
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- VeganLife/Views/Popups/BmiMoreInfoToolBarPopup.xaml.cs
+++ VeganLife/Views/Popups/BmiMoreInfoToolBarPopup.xaml.cs
@@ -1,14 +1,20 @@
 using CommunityToolkit.Maui.Views;
+using static VeganLife.Helpers.AppSetting.StaticHelper;
 
 namespace VeganLife.Views.Popups;
 
 public partial class BmiMoreInfoToolBarPopup : Popup
 {
+	public ObservableCollection<string> Documents { get; private set; }
 	public BmiMoreInfoToolBarPopup()
 	{
 		InitializeComponent();
 		mainGrid.WidthRequest = App.MainSize * 0.8;
-	}
+		Documents = new ObservableCollection<string>();
+		var document = HealthDiagnosisFirebaseDataModel.BMIModel?.Documents;
+		Documents.Add(document.WikiVN);
+		Documents.Add(document.WHO);
+    }
 
     private void Close_TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
