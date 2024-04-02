2166f8389efe86c14569235da2bc0881acf72582
thien <thiennguyen1007@gmail.com>
2024-04-03 00:04:17 +0700

    add send mail to request spuport

--- VeganLife/Services/DeviceService.cs
+++ VeganLife/Services/DeviceService.cs
@@ -20,5 +20,29 @@ namespace VeganLife.Services
 
         // public double WidthScreen => Application.Current?.MainPage?.Width ?? default;
         // public double HeightScreen => Application.Current?.MainPage?.Height ?? default;
+        public async Task SendEmailAsync(string subject, string body, List<string> recipients, List<string> CCrecipients = null)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                    Cc = CCrecipients,
+                    BodyFormat = EmailBodyFormat.PlainText // Use EmailBodyFormat.Html for HTML content
+                };
+
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
+        }
     }
 }
