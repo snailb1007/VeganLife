d6959418f6f6ac789618ff33644667801a2f9a51
thien <thiennguyen1007@gmail.com>
2024-04-14 22:59:50 +0700

    fixed keyStore

--- VeganLife/Views/Base/BasePage.cs
+++ VeganLife/Views/Base/BasePage.cs
@@ -31,7 +31,7 @@ namespace VeganLife.Views.Base
         {
             base.OnAppearing();
 #if DEBUG
-            Debug.WriteLine($"=> OnAppearing: {this.Title}");
+            Debug.WriteLine($"★ OnAppearing: {this.Title}");
 #endif
             MainThread.BeginInvokeOnMainThread(async () =>
             {
@@ -44,12 +44,20 @@ namespace VeganLife.Views.Base
         {
             base.OnDisappearing();
 #if DEBUG
-            Debug.WriteLine($"=> OnDisappearing: {this.Title}");
+            Debug.WriteLine($"★ OnDisappearing: {this.Title}");
 #endif
             MainThread.BeginInvokeOnMainThread(async () =>
             {
                 await (this.BindingContext as BaseViewModel)?.ViewDisappearingVM()!;
             });
+        }
+
+        protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+        {
+#if DEBUG
+            Debug.WriteLine($"★ OnNavigatedFrom: {this.Title}");
+#endif
+            base.OnNavigatedFrom(args);
         }
 
         protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action? onChanged = null)
