3956cd2739520bcbd147cc30fb8f536fd84ed551
thien <thiennguyen1007@gmail.com>
2024-05-04 13:12:55 +0700

    changed ui: filter in main page foods

--- VeganLife/ViewModels/MainViewModel.cs
+++ VeganLife/ViewModels/MainViewModel.cs
@@ -4,17 +4,14 @@
 
 namespace VeganLife.ViewModels
 {
-    using CommunityToolkit.Maui.Views;
     using CommunityToolkit.Mvvm.Messaging;
     using System.Text.RegularExpressions;
     using VeganLife.Data.LocalData;
     using VeganLife.Helpers;
     using VeganLife.Messages;
     using VeganLife.Models.FoodModel;
-    using VeganLife.Resources.Translations;
     using VeganLife.Services.UserServices;
     using VeganLife.Views.MainPageFlyout.FoodTab;
-    using VeganLife.Views.Popups;
 
     /// <summary>
     /// vm for MainPage.
