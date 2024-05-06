026aa04c3720161cfa9714b9c2250ee365cea4cc
thien <thiennguyen1007@gmail.com>
2024-05-06 21:54:18 +0700

    temp comment code, need more to check

--- VeganLife/AppShell.xaml.cs
+++ VeganLife/AppShell.xaml.cs
@@ -132,7 +132,7 @@ namespace VeganLife
                             => await vm.ViewIsRemovedAsync().ConfigureAwait(false)!);
                     }
 
-                    page.TearDown();
+                    // page.TearDown();
                 }
             }
 
