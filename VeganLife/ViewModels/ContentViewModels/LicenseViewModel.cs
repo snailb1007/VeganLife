77e9c416c1456585ea32144004a17548d3e862a1
thiennv <thiennv@vietis.com.vn>
2024-05-14 17:27:25 +0700

    update license

--- VeganLife/ViewModels/ContentViewModels/LicenseViewModel.cs
+++ VeganLife/ViewModels/ContentViewModels/LicenseViewModel.cs
@@ -24,7 +24,7 @@ namespace VeganLife.ViewModels.ContentViewModels
                 new LicenseModel("CommunityToolkit.Maui", "https://raw.githubusercontent.com/CommunityToolkit/Maui/main/LICENSE"),
                 new LicenseModel("CommunityToolkit.Mvvm", "https://raw.githubusercontent.com/CommunityToolkit/dotnet/main/License.md"),
                 new LicenseModel("FirebaseDatabase.net", "https://raw.githubusercontent.com/step-up-labs/firebase-database-dotnet/master/LICENSE"),
-                new LicenseModel("FFImageLoadingCompat.Maui", "https://raw.githubusercontent.com/Redth/FFImageLoading.Compat/main/LICENSE.md"),
+                new LicenseModel("FFImageLoading.Maui", "https://github.com/microspaze/FFImageLoading.Maui/blob/main/LICENSE.md"),
                 new LicenseModel("LiveChartsCore.SkiaSharpView.Maui", "https://raw.githubusercontent.com/beto-rodriguez/LiveCharts2/master/LICENSE"),
                 new LicenseModel("PureWeen.Maui.FixesAndWorkarounds", "https://raw.githubusercontent.com/PureWeen/ShanedlerSamples/main/LICENSE"),
                 new LicenseModel("sqlite-net-pcl", "https://raw.githubusercontent.com/praeclarum/sqlite-net/master/LICENSE.txt"),
@@ -32,6 +32,12 @@ namespace VeganLife.ViewModels.ContentViewModels
                 new LicenseModel("SQLitePCLRaw.core", "https://raw.githubusercontent.com/ericsink/SQLitePCL.raw/master/LICENSE.TXT"),
                 new LicenseModel("SQLitePCLRaw.provider.dynamic_cdecl", "https://raw.githubusercontent.com/ericsink/SQLitePCL.raw/master/LICENSE.TXT"),
                 new LicenseModel("SQLitePCLRaw.provider.e_sqlite3", "https://raw.githubusercontent.com/ericsink/SQLitePCL.raw/master/LICENSE.TXT"),
+                new LicenseModel("Sentry.Maui", "https://github.com/getsentry/sentry-dotnet/blob/main/LICENSE"),
+                new LicenseModel("akgul.Maui.DataGrid", "https://github.com/akgulebubekir/Maui.DataGrid/blob/main/LICENSE"),
+                new LicenseModel("CardsView.Maui", "https://github.com/AndreiMisiukevich/CardView.MAUI/blob/main/LICENSE"),
+                new LicenseModel("FirebaseDatabase.net", "https://github.com/step-up-labs/firebase-database-dotnet/blob/master/LICENSE"),
+                new LicenseModel("HtmlAgilityPack", "https://github.com/zzzprojects/html-agility-pack/blob/master/LICENSE"),
+                //new LicenseModel("Mopups", "https://github.com/zzzprojects/html-agility-pack/blob/master/LICENSE"),
             };
         }
 
