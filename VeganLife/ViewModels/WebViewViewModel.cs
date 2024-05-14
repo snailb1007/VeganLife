57b4cc894d89a1b6437332f51162fae64d786687
thiennv <thiennv@vietis.com.vn>
2024-05-14 17:26:46 +0700

    clean, uness

--- VeganLife/ViewModels/WebViewViewModel.cs
+++ VeganLife/ViewModels/WebViewViewModel.cs
@@ -1,76 +1,76 @@
-﻿// <copyright file="WebViewViewModel.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+﻿//// <copyright file="WebViewViewModel.cs" company="PlaceholderCompany">
+//// Copyright (c) PlaceholderCompany. All rights reserved.
+//// </copyright>
 
-using System.Threading.Tasks;
-using VeganLife.Resources.Translations;
+//using System.Threading.Tasks;
+//using VeganLife.Resources.Translations;
 
-namespace VeganLife.ViewModels
-{
-    /// <summary>
-    /// vm for WebViewPage.
-    /// </summary>
-    public partial class WebViewViewModel : BaseViewModel
-    {
-        [ObservableProperty]
-        private string sourceWeb;
+//namespace VeganLife.ViewModels
+//{
+//    /// <summary>
+//    /// vm for WebViewPage.
+//    /// </summary>
+//    public partial class WebViewViewModel : BaseViewModel
+//    {
+//        [ObservableProperty]
+//        private string sourceWeb;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WebViewViewModel"/> class.
-        /// </summary>
-        public WebViewViewModel()
-            : base()
-        {
-            // TODO: Update the default URL
-            this.SourceWeb = "https://veganhealthies.wordpress.com/";
-            this.IsLoading = true;
-        }
+//        /// <summary>
+//        /// Initializes a new instance of the <see cref="WebViewViewModel"/> class.
+//        /// </summary>
+//        public WebViewViewModel()
+//            : base()
+//        {
+//            // TODO: Update the default URL
+//            this.SourceWeb = "https://veganhealthies.wordpress.com/";
+//            this.IsLoading = true;
+//        }
 
-        [RelayCommand]
-        private async Task WebViewNavigated(WebNavigatedEventArgs e)
-        {
-            this.IsLoading = false;
-            if (e.Result != WebNavigationResult.Success)
-            {
-                // TODO: handle failed navigation in an appropriate way
-                await Shell.Current.DisplayAlert(AppResources.navigationFailed_alert_common, e.Result.ToString(), AppResources.ok_common);
-            }
-        }
+//        [RelayCommand]
+//        private async Task WebViewNavigated(WebNavigatedEventArgs e)
+//        {
+//            this.IsLoading = false;
+//            if (e.Result != WebNavigationResult.Success)
+//            {
+//                // TODO: handle failed navigation in an appropriate way
+//                await Shell.Current.DisplayAlert(AppResources.navigationFailed_alert_common, e.Result.ToString(), AppResources.ok_common);
+//            }
+//        }
 
-        [RelayCommand]
-        private void NavigateBack(WebView webView)
-        {
-            if (webView.CanGoBack)
-            {
-                webView.GoBack();
-            }
-        }
+//        [RelayCommand]
+//        private void NavigateBack(WebView webView)
+//        {
+//            if (webView.CanGoBack)
+//            {
+//                webView.GoBack();
+//            }
+//        }
 
-        [RelayCommand]
-        private void NavigateForward(WebView webView)
-        {
-            if (webView.CanGoForward)
-            {
-                webView.GoForward();
-            }
-        }
+//        [RelayCommand]
+//        private void NavigateForward(WebView webView)
+//        {
+//            if (webView.CanGoForward)
+//            {
+//                webView.GoForward();
+//            }
+//        }
 
-        [RelayCommand]
-        private void RefreshPage(WebView webView)
-        {
-            webView.Reload();
-        }
+//        [RelayCommand]
+//        private void RefreshPage(WebView webView)
+//        {
+//            webView.Reload();
+//        }
 
-        [RelayCommand]
-        private async Task OpenInBrowser()
-        {
-            await Launcher.OpenAsync(this.SourceWeb);
-        }
+//        [RelayCommand]
+//        private async Task OpenInBrowser()
+//        {
+//            await Launcher.OpenAsync(this.SourceWeb);
+//        }
 
-        [RelayCommand]
-        private void ShowMainMenu()
-        {
-            AppShell.ShowFlyOut();
-        }
-    }
-}
+//        [RelayCommand]
+//        private void ShowMainMenu()
+//        {
+//            AppShell.ShowFlyOut();
+//        }
+//    }
+//}
--- VeganLife/ViewModels/WelcomeViewModel.cs
+++ VeganLife/ViewModels/WelcomeViewModel.cs
@@ -1,86 +1,86 @@
-﻿namespace VeganLife.ViewModels
-{
-    public partial class WelcomeViewModel : BaseViewModel
-    {
-        [ObservableProperty]
-        private IList<TutorialModel> _listImage;
+﻿//namespace VeganLife.ViewModels
+//{
+//    public partial class WelcomeViewModel : BaseViewModel
+//    {
+//        [ObservableProperty]
+//        private IList<TutorialModel> _listImage;
 
-        [ObservableProperty]
-        private byte _flowViewSelectedIndex;
+//        [ObservableProperty]
+//        private byte _flowViewSelectedIndex;
 
-        public WelcomeViewModel()
-            : base()
-        {
-            Init();
-        }
+//        public WelcomeViewModel()
+//            : base()
+//        {
+//            Init();
+//        }
 
-        //public override async Task<Task> ViewAppearingVM()
-        //{
-            //var tutorialData = await Helpers.ResourceReader.ReadTextFileAsync("VeganLife.Resources.Raw.tutorial.txt");
-            //var items = StringProcessHelper.ParseSections(tutorialData);
-            //if (items != null && items.Any())
-            //{
-            //    for (int i = 0; i < 3; i++)
-            //    {
-            //        var target = ListImage.ElementAt(i);
-            //        var data = items.ElementAt(i);
-            //        target.Title = data.Title;
-            //        target.Description = data.Description;
-            //    }
-            //}
+//        //public override async Task<Task> ViewAppearingVM()
+//        //{
+//            //var tutorialData = await Helpers.ResourceReader.ReadTextFileAsync("VeganLife.Resources.Raw.tutorial.txt");
+//            //var items = StringProcessHelper.ParseSections(tutorialData);
+//            //if (items != null && items.Any())
+//            //{
+//            //    for (int i = 0; i < 3; i++)
+//            //    {
+//            //        var target = ListImage.ElementAt(i);
+//            //        var data = items.ElementAt(i);
+//            //        target.Title = data.Title;
+//            //        target.Description = data.Description;
+//            //    }
+//            //}
 
-            //if (UserSettingsHelper.IsFirstTime)
-            //{
-            //    UserSettingsHelper.SetAsync(UserSettingKey.IsFirstTime, false.ToString());
-            //    await Task.Delay(1);
-            //    var isCollectAccepted = await this.navigationService.DisplayAlert(
-            //        string.Empty,
-            //        message: AppResources.Alert_CollectOperationLogsPermission_Message,
-            //        ok: AppResources.ok_common,
-            //        cancel: AppResources.cancel_common);
-            //    UserSettingsHelper.SetAsync(UserSettingKey.IsAcceptedCollectLogs, isCollectAccepted.ToString());
-            //    App.Current.MainPage.ShowPopup(ServicesHelper.GetService<AboutAppPopup>());
-            //}
+//            //if (UserSettingsHelper.IsFirstTime)
+//            //{
+//            //    UserSettingsHelper.SetAsync(UserSettingKey.IsFirstTime, false.ToString());
+//            //    await Task.Delay(1);
+//            //    var isCollectAccepted = await this.navigationService.DisplayAlert(
+//            //        string.Empty,
+//            //        message: AppResources.Alert_CollectOperationLogsPermission_Message,
+//            //        ok: AppResources.ok_common,
+//            //        cancel: AppResources.cancel_common);
+//            //    UserSettingsHelper.SetAsync(UserSettingKey.IsAcceptedCollectLogs, isCollectAccepted.ToString());
+//            //    App.Current.MainPage.ShowPopup(ServicesHelper.GetService<AboutAppPopup>());
+//            //}
 
-            //App.SetupCollectLogPermission();
+//            //App.SetupCollectLogPermission();
 
-            //return base.ViewAppearingVM();
-        //}
+//            //return base.ViewAppearingVM();
+//        //}
 
-        private void Init()
-        {
-            //ListImage = new List<TutorialModel>
-            //{
-            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step1.webp?alt=media&token=f44e2020-0501-4de1-8fc5-5f1d3f37e88e" },
-            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step2.webp?alt=media&token=adf95f44-172c-47c1-b1fb-a6b893ee0263" },
-            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step3.webp?alt=media&token=9fc5072b-9e2d-4a34-b7bb-2c93ca6ad811" }
-            //};
-        }
+//        private void Init()
+//        {
+//            //ListImage = new List<TutorialModel>
+//            //{
+//            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step1.webp?alt=media&token=f44e2020-0501-4de1-8fc5-5f1d3f37e88e" },
+//            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step2.webp?alt=media&token=adf95f44-172c-47c1-b1fb-a6b893ee0263" },
+//            //    new TutorialModel() { ImageLink = "https://firebasestorage.googleapis.com/v0/b/vegan-life-d1c9b.appspot.com/o/step3.webp?alt=media&token=9fc5072b-9e2d-4a34-b7bb-2c93ca6ad811" }
+//            //};
+//        }
 
-        [RelayCommand]
-        private void Skip()
-        {
-            // if (Application.Current is not null)
-            // {
-            //    Application.Current.MainPage = new AppShell();
-            // }
-        }
+//        [RelayCommand]
+//        private void Skip()
+//        {
+//            // if (Application.Current is not null)
+//            // {
+//            //    Application.Current.MainPage = new AppShell();
+//            // }
+//        }
 
-        public class TutorialModel : BaseDataModel
-        {
-            public required string ImageLink { get; set; }
-        }
+//        public class TutorialModel : BaseDataModel
+//        {
+//            public required string ImageLink { get; set; }
+//        }
 
-        [RelayCommand]
-        private void ManualSwipeCard(string param)
-        {
-            // if (param.Equals("0") && FlowViewSelectedIndex > 0)
-            // {
-            //    FlowViewSelectedIndex--;
-            // }else if (param.Equals("1") && FlowViewSelectedIndex < ListImage.Count -1)
-            // {
-            //    FlowViewSelectedIndex++;
-            // }
-        }
-    }
-}
+//        [RelayCommand]
+//        private void ManualSwipeCard(string param)
+//        {
+//            // if (param.Equals("0") && FlowViewSelectedIndex > 0)
+//            // {
+//            //    FlowViewSelectedIndex--;
+//            // }else if (param.Equals("1") && FlowViewSelectedIndex < ListImage.Count -1)
+//            // {
+//            //    FlowViewSelectedIndex++;
+//            // }
+//        }
+//    }
+//}
