4cce3b461f3b5c1cc34ee3c5c40457c6069022ec
thien <thiennguyen1007@gmail.com>
2024-06-03 23:57:18 +0700

    add letter thank u

--- VeganLife/ViewModels/SupportPageVM.cs
+++ VeganLife/ViewModels/SupportPageVM.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VeganLife.Helpers;
+﻿using VeganLife.Helpers;
 using VeganLife.Helpers.AppSetting;
-using VeganLife.Resources.Translations;
 
 namespace VeganLife.ViewModels
 {
     public partial class SupportPageVM : BaseViewModel
     {
+        [ObservableProperty]
+        private string letter;
+
+        public override async Task ViewAppearingVM()
+        {
+            Letter ??= await ResourceReader.ReadTextFileAsync("VeganLife.Resources.Raw.letter_thankYou.txt");
+            await base.ViewAppearingVM();
+        }
+
         [RelayCommand]
         private async Task OpenCommunityAsync()
         {
