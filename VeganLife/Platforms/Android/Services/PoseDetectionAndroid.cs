8c6d6c63a6a257baefa48a36a5551b17bcf1e978
thiennv <thiennv@vietis.com.vn>
2024-06-07 09:05:42 +0700

    add pack, more base

--- VeganLife/Platforms/Android/Services/PoseDetectionAndroid.cs
+++ VeganLife/Platforms/Android/Services/PoseDetectionAndroid.cs
@@ -1,15 +1,19 @@
 ﻿using Xamarin.Google.MLKit.Vision.Pose;
 using Xamarin.Google.MLKit.Vision.Pose.Accurate;
+using Xamarin.Google.MLKit.Vision.Pose.Defaults;
 
 namespace VeganLife.Platforms.Android.Services
 {
     internal class PoseDetectionAndroid
     {
-        private PoseDetection _poseDetector;
-        private PoseDetectorOptionsBase _poseDetectorOptions;
+        private IPoseDetector _poseDetector;
 
         public PoseDetectionAndroid()
         {
+            var builder = new PoseDetectorOptions.Builder();
+            builder.SetDetectorMode(AccuratePoseDetectorOptions.StreamMode);
+            var poseDetector = builder.Build();
+            _poseDetector = PoseDetection.GetClient(poseDetector);
         }
     }
 }
