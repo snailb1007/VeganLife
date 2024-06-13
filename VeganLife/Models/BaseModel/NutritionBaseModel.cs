6cf58589c36e5af6b32e4b2dec86932e384c1fd4
thien <thiennguyen1007@gmail.com>
2024-06-13 23:56:55 +0700

    update & clean

---
+++ VeganLife/Models/BaseModel/NutritionBaseModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using SQLite;
+
+namespace VeganLife.Models.BaseModel
+{
+    public class NutritionBaseModel
+    {
+        [JsonIgnore]
+        [PrimaryKey]
+        public string Id { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+    }
+}
