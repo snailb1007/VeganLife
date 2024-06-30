14c9aac1dc593e01b608e7ae08805d5314b53483
thien <thiennguyen1007@gmail.com>
2024-06-30 18:45:18 +0700

    clean

--- VeganLife/AppShell.xaml.cs
+++ VeganLife/AppShell.xaml.cs
@@ -82,12 +82,12 @@ namespace VeganLife
                 case 0:
                     this.CurrentItem = mainTool_tool;
                     break;
-                case 1:
-                    this.CurrentItem = bmiCalculator_tool;
-                    break;
-                case 2:
-                    this.CurrentItem = bmrCalculator_tool;
-                    break;
+                //case 1:
+                //    this.CurrentItem = bmiCalculator_tool;
+                //    break;
+                //case 2:
+                //    this.CurrentItem = bmrCalculator_tool;
+                //    break;
             }
         }
 
