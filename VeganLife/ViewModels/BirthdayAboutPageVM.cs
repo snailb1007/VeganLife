bd94bc223337f702ea806543b9069b772c9cd597
thien <thiennguyen1007@gmail.com>
2024-07-21 23:06:03 +0700

    valid value for about you

--- VeganLife/ViewModels/BirthdayAboutPageVM.cs
+++ VeganLife/ViewModels/BirthdayAboutPageVM.cs
@@ -4,12 +4,38 @@ namespace VeganLife.ViewModels
 {
     public partial class BirthdayAboutPageVM : BaseViewModel
     {
-        public DateTime MaximumDateOfBirth => DateTime.Now.AddDays(-30);
+        private string _name;
+
+        public DateTime MaximumDateOfBirth => DateTime.Now.AddDays(-30).Date;
+
+        [ObservableProperty]
+        private DateTime selectedDate;
+
+        public bool IsFilledBirthday => SelectedDate < MaximumDateOfBirth && SelectedDate > DateTime.MinValue;
+
+        public override Task ViewAppearingVM()
+        {
+            SelectedDate = MaximumDateOfBirth;
+            return base.ViewAppearingVM();
+        }
+
+        public override Task OnNavigatingTo(object? parameter)
+        {
+            if (parameter is string name)
+            {
+                _name = name;
+            }
+
+            return base.OnNavigatingTo(parameter);
+        }
 
         [RelayCommand]
         public async Task OnContinueClicked()
         {
-            await this.navigationService.NavigateToPage<GenderAboutPage>();
+            await this.navigationService.NavigateToPage<GenderAboutPage>(
+                new InitAboutYouDataRecord(Name: _name, Birthday: SelectedDate, false));
         }
     }
+
+    internal record InitAboutYouDataRecord(string Name, DateTime Birthday, bool IsMale);
 }
