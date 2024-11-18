ae6f1a8be0adbd8226b0017577112038121a83e1
thiennv <thiennv@vietis.com.vn>
2024-11-18 18:03:18 +0700

    add miss logic

---
+++ VeganLife/Models/BaseModel/BaseNutrientModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using VeganLife.Helpers;
+
+namespace VeganLife.Models.BaseModel
+{
+    public abstract class BaseNutrientModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public string VietnameseName => StringProcessHelper.GetNameContainVietnameseTranslations(enName: this.Name ?? string.Empty);
+    }
+}
